3777e10301c25b6273b3879ac93b3680addf33d5
enotowitch <en.enotowitch@gmail.com>
2021-01-07 12:54:27 +0200

    added - destroy files from UPLOADS, then destroy DB record

--- destroy.php
+++ destroy.php
@@ -6,7 +6,7 @@ require 'DB.php';
 $post_id = $_REQUEST['hidden_id_delete'];
 $user_id = $_POST['user_id'];
 
-// ! first delete files, then delete DB record
+// ! first delete files form UPLOADS, then destroy DB record
 $delete_files = mysqli_query($connect, "SELECT * FROM `tbl_card` WHERE `tbl_card`.`job_post_id` = '$post_id' AND `user_id` = '$user_id'");
 
 $delete_files = mysqli_fetch_all($delete_files, MYSQLI_ASSOC);
